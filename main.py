2f5d38afbb542161b0340e7162bfdc2f5a93decc
Rolly Maulana Awangga <rolly@awang.ga>
2018-09-19 08:51:24 +0700

    python 3 compatible utf 8 and bytes converter

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from ctypes import *
 mylib=CDLL("./libflags.so")
 asalpesan="saya ganteng"
 mylib.welcome_msg(asalpesan.encode("ascii"))
+mylib.echo.restype = c_char_p
 pesan=mylib.echo(asalpesan.encode("ascii"))
 mylib.set_flag(777)
 bendera=mylib.get_flag()
-print(pesan)
+print(pesan.decode("utf-8"))
 print(bendera)
 
